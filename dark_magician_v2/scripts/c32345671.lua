da9dabf214989edf24d65deadb09d1fcfc8b1171
Carbony14 <83952723+Carbony14@users.noreply.github.com>
2025-05-20 14:27:00 +0100

    Updated Dark Magician V2 cards Fixed Preparation of Dark Magic

--- dark_magician_v2/scripts/c32345671.lua
+++ dark_magician_v2/scripts/c32345671.lua
@@ -11,6 +11,17 @@ function s.initial_effect(c)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
+
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(s.escon)
+	e2:SetTarget(s.estg)
+	e2:SetOperation(s.esop)
+	e2:SetCountLimit(1,id+100)
+	c:RegisterEffect(e2)
 end
 
 function s.filter(c)
@@ -38,7 +49,11 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			and Duel.IsExistingMatchingCard(s.dmfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) then
 			Duel.BreakEffect()
 			Duel.ShuffleDeck(tp)
-			Duel.Draw(tp,1,REASON_EFFECT)
+
+			if Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
+				Duel.Draw(tp,1,REASON_EFFECT)
+			end
+
 		end
 	end
 
@@ -46,4 +61,31 @@ end
 
 function s.dmfilter(c)
     return c:IsCode(CARD_DARK_MAGICIAN)
+end
+
+
+function s.esfilter(c)
+    return c:IsCode(48680970) and c:IsSSetable()
+end
+
+function s.escon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetLocationCount(tp, LOCATION_SZONE) > 0
+end
+
+function s.estg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+            and Duel.IsExistingMatchingCard(s.esfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+    end
+end
+
+function s.esop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.Remove(c,POS_FACEUP,REASON_EFFECT)~=0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+        local g=Duel.SelectMatchingCard(tp,s.esfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+        if #g>0 then
+            Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+        end
+    end
 end
